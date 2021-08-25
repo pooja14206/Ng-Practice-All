b54d00ee021d52be15679ad2a1d81d7f5a60424e
Pooja14206 <pooja.14206@gmail.com>
2021-08-25 14:26:29 +0530

    Added a file for String Interpolation

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  public serverId: number = 10;
+  private serverStatus: string = "offline";
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+  ngOnInit(): void {
+  }
+
+}
